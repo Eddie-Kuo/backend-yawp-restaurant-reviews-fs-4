6d1f764ee440c9165420c08b37373836c33a6892
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-28 16:51:31 -0600

    get all restaurants test written

---
+++ __tests__/restaurants.test.js
@@ -0,0 +1,25 @@
+const setup = require('../data/setup');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService'); depending if I put in the reviews testing here as well
+
+describe('restaurant routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  test('GET /api/v1/restaurants', async () => {
+    const res = await request(app).get('/api/v1/restaurants');
+    expect(res.body.length).toEqual(4);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      cuisine: expect.any(String),
+      cost: expect.any(Number),
+    });
+  });
+});
